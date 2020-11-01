82537440e0e7ff552272e5656367c51e8e02be5b
Hubert Krzemiński <229928@edu.p.lodz.pl>
2020-11-01 15:40:56 +0100

    Add BacktrackingSudokuSolver test

---
+++ Sudoku/src/test/java/com/mycompany/sudoku/BacktrackingSudokuSolverTest.java
@@ -0,0 +1,21 @@
+package com.mycompany.sudoku;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BacktrackingSudokuSolverTest {
+
+    @org.junit.jupiter.api.Test
+    public void solve() {
+        System.out.println("solve");
+        BacktrackingSudokuSolver testSudokuSolver = new BacktrackingSudokuSolver();
+        SudokuBoard instance = new SudokuBoard();
+        instance.fixedStart();
+
+        boolean expResult = true;
+        boolean result = testSudokuSolver.solve(instance);
+        assertEquals(expResult, result);
+
+    }
+}
