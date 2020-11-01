82e83861c86c518a96852aa7092e20a660be7a9f
229863 Deriu Markus <229863@edu.p.lodz.pl>
2020-11-01 13:18:48 +0100

    Poprawa skladni

--- Sudoku/src/main/java/com/mycompany/sudoku/BacktrackingSudokuSolver.java
+++ Sudoku/src/main/java/com/mycompany/sudoku/BacktrackingSudokuSolver.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.sudoku;
 
-/**
- *
- * @author student
- */
 public class BacktrackingSudokuSolver implements SudokuSolver {
-    public boolean solve(SudokuBoard board){
+    public boolean solve(SudokuBoard board) {
         for (int row = 0;row < 9;row++) {
         for (int col = 0;col < 9;col++) {
             if (board.board[col][row] == 0) {
