b1f478fe93d07c4a519596ac7c26eb1c1b267d60
Hubert Krzemiński <229928@edu.p.lodz.pl>
2020-11-05 18:08:07 +0100

    Implement SudokuColumn //WIP

--- Sudoku/src/main/java/com/mycompany/sudoku/SudokuColumn.java
+++ Sudoku/src/main/java/com/mycompany/sudoku/SudokuColumn.java
@@ -1,4 +1,25 @@
 package com.mycompany.sudoku;
 
 public class SudokuColumn {
+
+    private SudokuField[] Column = new SudokuField[9];
+
+    public SudokuColumn(int y) {
+        for (int i=0;i<9;i++) {
+            Column[i]=field[y][i];
+        }
+    }
+
+    public boolean verify() {
+
+
+        //column
+        for (int y = 0;y < 9;y++) {
+            if (get(column,y) == i) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
