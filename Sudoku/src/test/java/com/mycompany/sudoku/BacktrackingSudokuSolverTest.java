d149aee3cba055033eba3500387098225aae995c
229863 Deriu Markus <229863@edu.p.lodz.pl>
2020-11-07 19:15:48 +0100

    Refaktoryzacja testów

--- Sudoku/src/test/java/com/mycompany/sudoku/BacktrackingSudokuSolverTest.java
+++ Sudoku/src/test/java/com/mycompany/sudoku/BacktrackingSudokuSolverTest.java
@@ -1,10 +1,12 @@
 package com.mycompany.sudoku;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.Random;
 
 class BacktrackingSudokuSolverTest {
 
+    Random random = new Random();
+    
     public void printSudoku(SudokuBoard board) {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
@@ -14,6 +16,50 @@ class BacktrackingSudokuSolverTest {
         }
     }
 
+    public void randomStart(SudokuBoard board) {
+
+        //ustawianie losowych liczb w losowych komórkach
+        for (int r = 0; r < 9; r++) {
+            int rr = random.nextInt(9);
+            int rc = random.nextInt(9);
+            //board[rc][rr] = random.nextInt(9) + 1;
+            int randomOdd = random.nextInt(9) + 1;
+            if (check(rr, rc, randomOdd, board)) {
+                board.set(rc, rr, randomOdd);
+            }
+        }
+}
+    
+    private boolean check(int row, int column, int i, SudokuBoard board) {
+
+        //row
+        for (int x = 0;x < 9;x++) {
+            if (board.get(x,row) == i) {
+                return false;
+            }
+        }
+
+        //column
+        for (int y = 0;y < 9;y++) {
+            if (board.get(column,y) == i) {
+                return false;
+            }
+        }
+
+        //3x3
+        int rzad = (row / 3) * 3;
+        int kol = (column / 3) * 3;
+        for (int m = rzad; m < rzad + 3; m++) {
+            for (int n  = kol;n < kol + 3; n++) {
+                if (board.get(n,m) == i) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+    
     public void fixedStart(SudokuBoard board) {
         //zainicjowanie zerami
         for (int i = 0; i < 9; i++) {
@@ -40,7 +86,7 @@ class BacktrackingSudokuSolverTest {
         //SudokuField field1 = new SudokuField();
         //System.out.println(field1.getFieldValue());
         //System.out.println(instance.board[0][0].getFieldValue());
-        instance.randomStart();
+        randomStart(instance);
         //fixedStart(instance);
         System.out.println("po random starcie:");
         printSudoku(instance);
--- Sudoku/src/test/java/com/mycompany/sudoku/SudokuBoardTest.java
+++ Sudoku/src/test/java/com/mycompany/sudoku/SudokuBoardTest.java
@@ -6,7 +6,9 @@ package com.mycompany.sudoku;
  * and open the template in the editor.
  */
 
+import java.lang.reflect.Method;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.Random;
 
 /**
  *
@@ -17,6 +19,51 @@ public class SudokuBoardTest {
     public SudokuBoardTest() {
     }
 
+Random random = new Random(); 
+    
+    public void randomStart(SudokuBoard board) {
+
+        //ustawianie losowych liczb w losowych komórkach
+        for (int r = 0; r < 9; r++) {
+            int rr = random.nextInt(9);
+            int rc = random.nextInt(9);
+            //board[rc][rr] = random.nextInt(9) + 1;
+            int randomOdd = random.nextInt(9) + 1;
+            if (check(rr, rc, randomOdd, board)) {
+                board.set(rc, rr, randomOdd);
+            }
+        }
+}
+    private boolean check(int row, int column, int i, SudokuBoard board) {
+
+        //row
+        for (int x = 0;x < 9;x++) {
+            if (board.get(x,row) == i) {
+                return false;
+            }
+        }
+
+        //column
+        for (int y = 0;y < 9;y++) {
+            if (board.get(column,y) == i) {
+                return false;
+            }
+        }
+
+        //3x3
+        int rzad = (row / 3) * 3;
+        int kol = (column / 3) * 3;
+        for (int m = rzad; m < rzad + 3; m++) {
+            for (int n  = kol;n < kol + 3; n++) {
+                if (board.get(n,m) == i) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+    
     public void fixedStart(SudokuBoard board) {
         //zainicjowanie zerami
         for (int i = 0; i < 9; i++) {
@@ -25,9 +72,9 @@ public class SudokuBoardTest {
             }
         }
 
-        board.set(2,1,3);
-        board.set(4,2,6);
-        board.set(0,0,8);
+       // board.set(2,1,3);
+       // board.set(4,2,6);
+       // board.set(0,0,8);
     }
 
     public void printSudoku(SudokuBoard board) {
@@ -48,7 +95,7 @@ public class SudokuBoardTest {
         System.out.println("solveGame");
         BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
         SudokuBoard instance = new SudokuBoard(solver);
-        instance.randomStart();
+        randomStart(instance);
         boolean expResult = true;
         boolean Result = instance.solveGame();
 
@@ -65,7 +112,7 @@ public class SudokuBoardTest {
         System.out.println("equalSum");
         BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
         SudokuBoard instance = new SudokuBoard(solver);
-        instance.randomStart();
+        randomStart(instance);
         //instance.printSudoku();
         
         int expResult = 405;
@@ -86,10 +133,10 @@ public class SudokuBoardTest {
     }
     
     /**
-     * Test of check method when false, of class SudokuBoard.
+     * Test of checkBoard method when false, of class SudokuBoard.
      */
     @org.junit.jupiter.api.Test
-    public void testCheck_false() {
+    public void testCheckBoard_false_row() {
         System.out.println("check_false");
         int row = 0;
         int column = 0;
@@ -98,17 +145,73 @@ public class SudokuBoardTest {
         SudokuBoard instance = new SudokuBoard(solver);
         fixedStart(instance);
         
-        boolean expResult = false;
+        boolean expResult = false;   
         boolean result = instance.isCheckBoardTrue();
+     
+        assertEquals(expResult, result);
+        
+    }
+    
+    @org.junit.jupiter.api.Test
+    public void testCheckBoard_false_Column() {
+        System.out.println("check_false");
+        int row = 0;
+        int column = 0;
+        int i = 0;
+        BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
+        SudokuBoard instance = new SudokuBoard(solver);
+        fixedStart(instance);
+        for(int j =1; j<10;j++){
+            instance.set(i,0,j);
+            i++;
+        }
+        
+    
+        
+        boolean expResult = false;   
+        boolean result = instance.isCheckBoardTrue();
+     
+        assertEquals(expResult, result);
+        
+    }
+    
+    @org.junit.jupiter.api.Test
+    public void testCheckBoard_false_Box() {
+        System.out.println("check_false");
+        int row = 0;
+        int column = 0;
+        int i = 0;
+        int m = 0 ;
+        BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
+        SudokuBoard instance = new SudokuBoard(solver);
+        fixedStart(instance);
+        for(int j =1; j<10;j++){
+            instance.set(i,0,j);
+            i++;
+        }
+        
+        for(int n=1;n<10;n++) {
+            instance.set(0,m,n);
+            m++;
+        }
+        
+        instance.set(2,1,0);
+    
+    
+    
+        
+        boolean expResult = false;   
+        boolean result = instance.isCheckBoardTrue();
+     
         assertEquals(expResult, result);
         
     }
     
     /**
-     * Test of check method when true, of class SudokuBoard.
+     * Test of checkBoard method when true, of class SudokuBoard.
      */
     @org.junit.jupiter.api.Test
-    public void testCheck_true() {
+    public void testCheckBoard_true() {
         System.out.println("check_true");
         int row = 0;
         int column = 0;
@@ -117,6 +220,7 @@ public class SudokuBoardTest {
         SudokuBoard instance = new SudokuBoard(solver);
         fixedStart(instance);
         instance.solveGame();
+        printSudoku(instance);
         
         boolean expResult = true;
         boolean result = instance.isCheckBoardTrue();
@@ -124,10 +228,11 @@ public class SudokuBoardTest {
         
     }
     
+    
     /**
      * Test of randomStart method, of class SudokuBoard.
      */
-    @org.junit.jupiter.api.Test
+   /* @org.junit.jupiter.api.Test
     public void testRandomStart() {
         System.out.println("randomStart");
         BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
@@ -147,16 +252,17 @@ public class SudokuBoardTest {
         
         assertEquals(expResult, check);
     }
+    */
 
     /**
-     * Test of getCell method, of class SudokuBoard.
+     * Test of get method, of class SudokuBoard.
      */
     @org.junit.jupiter.api.Test
-    public void testGetCell() {
+    public void testGet() {
         System.out.println("getCell");
         BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
         SudokuBoard instance = new SudokuBoard(solver);
-        instance.randomStart();
+        randomStart(instance);
         instance.set(2,3,3);
         
         int check=instance.get(2, 3);
@@ -177,10 +283,10 @@ public class SudokuBoardTest {
         BacktrackingSudokuSolver solver2 = new BacktrackingSudokuSolver();
         
         SudokuBoard instance1 = new SudokuBoard(solver1);
-        instance1.randomStart();
+        randomStart(instance1);
         
         SudokuBoard instance2 = new SudokuBoard(solver2);
-        instance2.randomStart();
+        randomStart(instance2);
         
         boolean check = false;
         boolean expResult = true;
@@ -199,12 +305,97 @@ public class SudokuBoardTest {
 
 
     @org.junit.jupiter.api.Test
-    void testSetCell() {
+    void testSet() {
         System.out.println("setCell");
         BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
         SudokuBoard instance = new SudokuBoard(solver);
         instance.set(6,6,6);
 
         assertEquals(instance.get(6,6), 6);
+    }
+    
+    @org.junit.jupiter.api.Test
+    void testGetBox_true() {
+        System.out.println("getBox_true");
+        BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
+        SudokuBoard instance = new SudokuBoard(solver);
+        fixedStart(instance);
+        instance.solveGame();
+        
+        boolean expResult = true;
+        
+        boolean result = instance.getBox(0, 0).verify();
+
+        assertEquals(result, expResult);
+    }
+    
+    @org.junit.jupiter.api.Test
+    void testGetBox_false() {
+        System.out.println("getBox_false");
+        BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
+        SudokuBoard instance = new SudokuBoard(solver);
+        fixedStart(instance);
+
+        boolean expResult = false;
+        
+        boolean result = instance.getBox(0, 0).verify();
+
+        assertEquals(result, expResult);
+    }
+    
+    @org.junit.jupiter.api.Test
+    void testGetRow_false() {
+        System.out.println("getRow_false");
+        BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
+        SudokuBoard instance = new SudokuBoard(solver);
+        fixedStart(instance);
+        
+        
+        instance.set(2,1,0);
+        boolean expResult = false;
+
+        assertEquals(instance.getRow(0).verify(), expResult);
+    }
+    
+    @org.junit.jupiter.api.Test
+    void testGetRow_true() {
+        System.out.println("getRow_true");
+        BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
+        SudokuBoard instance = new SudokuBoard(solver);
+        fixedStart(instance);
+        instance.solveGame();
+        
+        
+        boolean expResult = true;
+
+        assertEquals(instance.getRow(0).verify(), expResult);
+    }
+    
+    @org.junit.jupiter.api.Test
+    void testGetColumn_false() {
+        System.out.println("getColumn_false");
+        BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
+        SudokuBoard instance = new SudokuBoard(solver);
+        fixedStart(instance);
+        
+        
+        instance.set(2,1,0);
+        boolean expResult = false;
+
+        assertEquals(instance.getColumn(0).verify(), expResult);
+    }
+    
+    @org.junit.jupiter.api.Test
+    void testGetColumn_true() {
+        System.out.println("getColumn_true");
+        BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
+        SudokuBoard instance = new SudokuBoard(solver);
+        fixedStart(instance);
+        instance.solveGame();
+        
+        
+        boolean expResult = true;
+
+        assertEquals(instance.getColumn(0).verify(), expResult);
     }
 }
