eec2eb7756cef2e3dd376302bf5fed684cf9bd81
Hubert Krzemiński <229928@edu.p.lodz.pl>
2020-11-07 12:20:54 +0100

    Implement SudokuBox

--- Sudoku/src/main/java/com/mycompany/sudoku/SudokuBox.java
+++ Sudoku/src/main/java/com/mycompany/sudoku/SudokuBox.java
@@ -1,4 +1,39 @@
 package com.mycompany.sudoku;
 
 public class SudokuBox {
+
+    private final SudokuField[] box = new SudokuField[9];
+
+    public SudokuBox(SudokuField[] field) {
+
+        for (int i = 0; i < 9; i++) {
+            box[i] = new SudokuField();
+            box[i].setFieldValue(field[i].getFieldValue());
+            }
+        }
+
+    public boolean verify() {
+
+        //3x3
+        int sum = 0;
+
+        //int [] checkBoard = new int[10];
+
+        for (int i = 0; i < 9; i++) {
+                sum += box[i].getFieldValue();
+                //checkBoard[Box[i][j].getFieldValue()]++;
+            }
+
+        //for (int i = 1; i < 10; i++) {
+        // if (checkBoard[i] != 1) {
+        //    return false;
+        // }
+        //}
+
+        if (sum != 45) {
+            return false;
+        }
+
+        return true;
+    }
 }
