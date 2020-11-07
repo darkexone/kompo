43103392328f1aacf99debe141140f96e48ebdf9
Hubert Krzemiński <229928@edu.p.lodz.pl>
2020-11-07 12:21:24 +0100

    Implement SudokuRow

--- Sudoku/src/main/java/com/mycompany/sudoku/SudokuRow.java
+++ Sudoku/src/main/java/com/mycompany/sudoku/SudokuRow.java
@@ -1,4 +1,32 @@
 package com.mycompany.sudoku;
 
 public class SudokuRow {
+
+    private SudokuField[] row = new SudokuField[9];
+
+    public SudokuRow(SudokuField[] field) {
+
+        for (int i = 0; i < 9; i++) {
+            row[i] = new SudokuField();
+        }
+
+        for (int i = 0; i < 9; i++) {
+            row[i].setFieldValue(field[i].getFieldValue());
+        }
+    }
+
+    public boolean verify() {
+
+        //Row
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            sum += row[i].getFieldValue();
+        }
+
+        if (sum == 45) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
