9e246ec6ca9ef7781d27d71ca37bcbb43c635a90
Hubert Krzemiński <229928@edu.p.lodz.pl>
2020-11-30 20:43:05 +0100

    Refactore FileSudokuBoardDao

--- Sudoku/src/main/java/com/mycompany/sudoku/FileSudokuBoardDao.java
+++ Sudoku/src/main/java/com/mycompany/sudoku/FileSudokuBoardDao.java
@@ -1,39 +1,53 @@
 package com.mycompany.sudoku;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.junit.platform.commons.util.ToStringBuilder;
 
-public class FileSudokuBoardDao implements Dao<SudokuBoard> { //TODO ,AutoCloseable { <-- co z tym?
+public class FileSudokuBoardDao implements Dao<SudokuBoard> {
 
     private String filename;
     private String absolutePath;
 
     public FileSudokuBoardDao(String filename) {
         this.filename = filename;
-        absolutePath = System.getProperty("user.home") + File.separator + this.filename;
+        this.absolutePath = System.getProperty("user.dir") + File.separator + this.filename;
+        // .. + "Serializable" + File.separator + this.filename;
     }
 
     @Override
-    public SudokuBoard read() {
+    public SudokuBoard read() throws Throwable {
         SudokuBoard sudokuBoardInstance = null;
 
-        try (FileReader fileReader = new FileReader(absolutePath)) {
-            int ch = fileReader.read();
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
-                    sudokuBoardInstance.set(i,j,ch);
-                }
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        try (FileInputStream fileIn = new FileInputStream(absolutePath);
+             ObjectInputStream in = new ObjectInputStream(fileIn)) {
+
+            sudokuBoardInstance = (SudokuBoard) in.readObject();
+            System.out.println("read file: " + absolutePath);
         }
 
         return sudokuBoardInstance;
+    }
+
+    @Override
+    public void write(SudokuBoard sudokuBoardInstance) throws Throwable {
+
+        try (FileOutputStream fileOut = new FileOutputStream(absolutePath);
+             ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+
+            out.writeObject(sudokuBoardInstance);
+            System.out.println("write file: " + absolutePath);
+        }
+    }
+
+    @Override
+    public void finalize() throws Throwable {
+        new FileInputStream(absolutePath).close();
     }
 
     @Override
