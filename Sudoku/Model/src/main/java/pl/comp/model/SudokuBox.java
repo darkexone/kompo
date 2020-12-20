8078da902fcdd78f89358ecee81b946c3f59a896
Hubert Krzemiński <229928@edu.p.lodz.pl>
2020-12-20 17:38:23 +0100

    Implement getList() in SudokuBox

--- Sudoku/Model/src/main/java/pl/comp/model/SudokuBox.java
+++ Sudoku/Model/src/main/java/pl/comp/model/SudokuBox.java
@@ -4,6 +4,10 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.junit.platform.commons.util.ToStringBuilder;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class SudokuBox implements Cloneable {
 
     private final SudokuField[] box = new SudokuField[9];
@@ -89,4 +93,8 @@ public class SudokuBox implements Cloneable {
 
     }
 
+    //TODO czy tak moze byc?
+    public List<SudokuField> getList() {
+        return Collections.unmodifiableList(Arrays.asList(box.clone()));
+    }
 }
